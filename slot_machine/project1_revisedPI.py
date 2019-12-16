5d82694463dea70aa0d42fa50feee46e7f94e8dd
alora1976 <39773598+alora1976@users.noreply.github.com>
2019-12-15 20:29:22 -0800

    Update project1_revisedPI.py

--- slot_machine/project1_revisedPI.py
+++ slot_machine/project1_revisedPI.py
@@ -112,7 +112,7 @@ def show_go_screen():#start screen
     draw_text(screen, "Press BUTTON to SPIN", 50,
               WIDTH / 2, HEIGHT / 2)
 
-        
+
 #initialize pygame and create window
 
 all_sprites = pygame.sprite.Group()
@@ -122,129 +122,7 @@ reel3=Reel(653,300)
 reel4=Reel(986,300)
 all_sprites.add(reel1,reel2,reel3,reel4)
 
-def play_bonus():
-    running=True
-    bank=0
-    timer=0
-    game_state=0
-    show_bonus_screen()
-    
-    while running:
-    
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            #check for closing window
-            if event.type==pygame.QUIT:
-                running=False
-            #if GPIO.event_detected(18):
-            if event.type == pygame.KEYUP:
-                
-                if game_state == 0:
-                    pygame.mixer.stop()
-                    game_state = 1
-                
-                    reel1.state = 1
-                    reel2.state = 1
-                    reel3.state = 1
-                    reel4.state = 1
-          
-             #update
-        all_sprites.update()
-        
-         #draw and render       
-    
-        
-       
-        if game_state == 0:
-             
-            show_bonus_screen()#change to bonus screen create def
-            begin = pygame.mixer.Sound(os.path.join(snd_dir,"bonus.wav"))
-            begin.play()
-            
-        elif game_state == 1:
-            
-            timer=timer+1
-            spin_sound=pygame.mixer.Sound(os.path.join(snd_dir,"spinning.wav"))
-            spin_sound.play()
-            
-            if timer == 100:
-                reel1.state=0
-            if timer==120:
-                reel2.state=0
-            if timer==140:
-                reel3.state=0
-            if timer==160:
-                reel4.state=0
-                game_state = 2
-                pygame.mixer.stop()
-                
-        elif game_state == 2:
-            reel1.state = 2
-            reel2.state = 2
-            reel3.state = 2
-            reel4.state = 2
-        
-            if (reel1.display==("book.png") and reel2.display==("book.png") and reel3.display==("book.png") and reel4.display==("book.png")):
-                
-                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
-                you_win.play()
-                bank=bank+10000
-                game_state = 5
-                
-            elif (reel1.display==reel2.display==reel3.display==reel4.display):
-                
-                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
-                you_win.play()
-                bank=bank+1000
-                game_state = 5
-                
-            elif (reel2.display==reel3.display==reel4.display):
-                
-                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
-                you_win.play()
-                bank=bank+100
-                game_state = 5
-                
-                
-            elif (reel1.display==reel2.display==reel3.display):
-                
-                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
-                you_win.play()
-                bank=bank+100
-                game_state = 5
-                
-            elif (reel1.display==reel3.display==reel4.display):
-                
-                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
-                you_win.play()
-                bank=bank+100
-                game_state = 5
-                
-            elif (reel1.display==reel2.display==reel4.display):
-                
-                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
-                you_win.play()
-                bank=bank+100
-                game_state = 5     
-                
-            elif (reel1.display=="wolf.jpg" or reel2.display=="wolf.jpg" or reel3.display=="wolf.jpg" or reel4.display=="wolf.jpg"):
-                you_lose=pygame.mixer.Sound(os.path.join(snd_dir,"game_over.wav"))
-                you_lose.play()
-                game_state=3
-                
-            else:
-                
-                you_lose=pygame.mixer.Sound(os.path.join(snd_dir,"game_over.wav"))
-                you_lose.play()
-                game_state=5
-            
-        elif game_state == 5:
-           
-            #RESET EVERYTHING
-            game_state = 0
-            timer = 0          
-           
-          #after drawing everything flip display again?????How do I makwe bank a global var???
+
    
        
 #game loop
@@ -360,28 +238,82 @@ while running:
         elif reel4.display==("book.png"):
             bonus=pygame.mixer.Sound(os.path.join(snd_dir,"bonus.wav"))
             bonus.play()
-            game_state=4
-            if game_state==4:
-                play_bonus()
-                    
-                    
-                    
-                    
+            game_state= 4
+            
+           
         else:
             
             you_lose=pygame.mixer.Sound(os.path.join(snd_dir,"game_over.wav"))
             you_lose.play()
             bank=bank-bet
             game_state = 3
-              
-            
-    
+                              
+                            
+        
     elif game_state == 3:
         #RESET EVERYTHING
         game_state = 0
         timer = 0
         
-
+    elif game_state==4:
+        game_state=0
+        timer=0
+        show_bonus_screen()
+ 
+        if (reel1.display==("book.png") and reel2.display==("book.png") and reel3.display==("book.png") and reel4.display==("book.png")):
+                
+                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
+                you_win.play()
+                bank=bank+10000
+                game_state = 4
+                    
+        elif (reel1.display==reel2.display==reel3.display==reel4.display):
+                
+                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
+                you_win.play()
+                bank=bank+1000
+                game_state = 4
+                
+        elif (reel2.display==reel3.display==reel4.display):
+                
+                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
+                you_win.play()
+                bank=bank+100
+                game_state = 4
+                
+                
+        elif (reel1.display==reel2.display==reel3.display):
+                
+                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
+                you_win.play()
+                bank=bank+100
+                game_state = 4
+                
+        elif (reel1.display==reel3.display==reel4.display):
+                
+                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
+                you_win.play()
+                bank=bank+100
+                game_state = 4
+                
+        elif (reel1.display==reel2.display==reel4.display):
+                
+                you_win=pygame.mixer.Sound(os.path.join(snd_dir,"jackpot.wav"))
+                you_win.play()
+                bank=bank+100
+                game_state = 4     
+                
+        elif (reel1.display=="wolf.jpg" or reel2.display=="wolf.jpg" or reel3.display=="wolf.jpg" or reel4.display=="wolf.jpg"):
+                    you_lose=pygame.mixer.Sound(os.path.join(snd_dir,"game_over.wav"))
+                    you_lose.play()
+                    game_state=3
+                
+                
+        else:
+                
+                you_lose=pygame.mixer.Sound(os.path.join(snd_dir,"game_over.wav"))
+                you_lose.play()
+                game_state = 4
         
     #after drawing everything flip display
     
